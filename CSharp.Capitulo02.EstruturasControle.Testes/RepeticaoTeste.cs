88f3bbc0292f3335059ae82ef17366baaab72a12
Vitor Neil Avelino <avelino.vitor@gmail.com>
2021-04-20 22:47:34 -0300

    Testes de unidade - repetição

---
+++ CSharp.Capitulo02.EstruturasControle.Testes/RepeticaoTeste.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp.Capitulo02.EstruturasControle.Testes
+{
+    [TestClass]
+    public class RepeticaoTeste
+    {
+        [TestMethod]
+        public void TabuadaTeste()
+        {
+
+        }
+    }
+}
